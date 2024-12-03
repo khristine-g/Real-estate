c73147e883374552c40ea74b7136718faf66a37f
khristine-g <githigekhristine@gmail.com>
2024-12-03 14:20:41 +0300

    Adding routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+   # Allow HEAD request to root
+   root to: -> { [200, {}, ['']] }
  
   resources :listings, only: [:index, :show]
   resources :agents, only: [:index, :show]
